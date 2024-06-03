8ddf6b5c2339dceec82ef08e60d568276f68c4de
kiieeaa <kiyaadinda5@gmail.com>
2024-06-03 10:25:08 +0700

    penanda 1

--- iofileWithPath/iofileWithPath.cpp
+++ iofileWithPath/iofileWithPath.cpp
@@ -19,7 +19,39 @@ int main() {
 
 
 	while (true) {
+		cout << "- ";
+
+		getline(cin, baris);
+
+		if (baris == "q") break;
+
+		outfile << baris << endl;
 
 	}
+	outfile.close();
 
+
+	ifstream infile;
+
+
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= membuka dan membaca file " << endl;
+
+		if (infile.is_open())
+		{
+
+			while (getline(infile, baris))
+			{
+
+				cout << baris << '\n';
+			}
+
+			infile.close();
+		}
+
+		else cout << "unable to open file";
+
+	return 0;
 }
+
