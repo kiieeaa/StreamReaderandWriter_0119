606b42bea15e730e4d795bcaafd61cc9f14a47ad
kiieeaa <kiyaadinda5@gmail.com>
2024-06-03 10:34:54 +0700

    melemparkan sebuah integer

---
+++ manualException/manualException.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+int main()
+{
+	try {
+		cout << "Selamat Belajar di prodi TI UMY" << endl;
+
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+}
